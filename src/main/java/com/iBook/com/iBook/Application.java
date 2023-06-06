39faaa487304713043578850ba3924fe768e6be8
Keilyn Aguero C <90811028+keilynagc@users.noreply.github.com>
2023-06-06 13:41:04 -0600

    Home page y estilos

---
+++ src/main/java/com/iBook/com/iBook/Application.java
@@ -0,0 +1,13 @@
+package com.iBook.com.iBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/iBook/com/iBook/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iBook.com.iBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
